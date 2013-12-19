d6b9dbc603706205837cbc8eec001fe258377350
Hayden Nhan <hdnhan@gmail.com>
2013-12-18 16:58:28 -0800

    created rails blog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Theotherday::Application.config.secret_key_base = '0d6908ca9728cb30fb2543f20ef2f111705de39d966e5f439707fbf0aef3f3545a592e4d7f5c5b2b23ed913f575d0b38ae0e65e2a40f29b8441d481baac0b1cc'
